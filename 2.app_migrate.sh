d291b9466126a865c0a95378612dccde85d57d08
dawright22 <53329137+dawright22@users.noreply.github.com>
2020-07-06 16:16:13 +1000

    Update 2.app_migrate.sh

--- 2.app_migrate.sh
+++ 2.app_migrate.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -v
 
- cd app_stack/app_AKS/
-
  kubectl config use-context $(grep 'aks$' KCONFIG.txt)
+
+ cd app_stack/app_AKS/
 
  ./full_stack_deploy.sh
 
@@ -21,6 +21,6 @@ set -v
 
  sleep 1
 
- echo "mesh complete"
+ echo "app migrated"
 
  cd ../../
