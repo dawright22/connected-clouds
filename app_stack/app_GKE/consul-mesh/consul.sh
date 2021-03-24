704d6328816eddad958863f877e347de6959a903
dawright22 <53329137+dawright22@users.noreply.github.com>
2021-03-24 12:32:53 +1100

    Create consul.sh

--- app_stack/app_GKE/consul-mesh/consul.sh
+++ app_stack/app_GKE/consul-mesh/consul.sh
@@ -12,7 +12,7 @@ echo "Installing consul using latest helm chart "
 helm install consul hashicorp/consul -f values.yaml #--debug
 
 echo "As this is the primary datacenter for federation, fetch the federation secret and store in local file.."
-kubectl create secret generic consul-gossip-encryption-key > consul-federation-secret.yaml
+kubectl get secret consul-gossip-encryption-key -o yaml > consul-federation-secret.yaml
 
 
 echo "Configuring Kube to forward consul DNS to consul..."
